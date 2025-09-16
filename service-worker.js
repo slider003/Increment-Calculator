8de601917f388815adfe7eb6f7a345a364d0a1f4
Matthew Coleman <124199027+slider003@users.noreply.github.com>
2025-09-16 21:34:01 +0000

    update service workers

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'date-calculator-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  './',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 // Install event - cache resources
